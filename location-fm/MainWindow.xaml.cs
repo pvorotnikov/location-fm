b5c9daa8bfaf497cfde689f1f56ec8c512a1f02d
Petar Vorotnikov <pvorotni@visteon.com>
2016-01-30 00:00:37 +0200

    Added proper Twitter requests on button click.

--- location-fm/MainWindow.xaml.cs
+++ location-fm/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LinqToTwitter;
+using location_fm.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,20 +25,43 @@ namespace location_fm
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new TweetViewModel();
         }
 
         void Window_Loaded(object sender, RoutedEventArgs e)
         {
             if (SharedState.Authorizer == null)
+            {
                 new OAuth().Show();
+            }
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void refreshTweetsBtn_Click(object sender, RoutedEventArgs e)
         {
-            var type = Type.GetType((sender as MenuItem).Tag as string);
-            Window formInst = (Window)Activator.CreateInstance(type);
+            refreshTweets();
+        }
 
-            formInst.Show();
+        // refresh the tweets
+        async void refreshTweets()
+        {
+            var twitterCtx = new TwitterContext(SharedState.Authorizer);
+
+            var tweets =
+                await
+                (from tweet in twitterCtx.Status
+                 where tweet.Type == StatusType.Home
+                 select new Tweet
+                 {
+                     ImageSource = tweet.User.ProfileImageUrl,
+                     UserName = tweet.User.ScreenNameResponse,
+                     Message = tweet.Text
+                 })
+                .ToListAsync();
+
+            var tweetCollection = (DataContext as TweetViewModel).Tweets;
+            tweetCollection.Clear();
+            tweets.ForEach(tweet => tweetCollection.Add(tweet));
         }
     }
 }
